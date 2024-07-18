76d50c8d1a0b82a20cd41652a7a5d302338f814a
HC2084 <hc14307@gmail.com>
2024-07-18 13:50:48 -0500

    Edited gurobipy portion to be valid with any string input as function

--- demo/models/submethods/lyapunov.py
+++ demo/models/submethods/lyapunov.py
@@ -3,7 +3,6 @@ import numpy as np
 import math
 # from scipy.optimize import minimize
 import scipy.optimize as opt
-<<<<<<< HEAD
 import gurobipy as gp
 from gurobipy import GRB
 from gurobipy import quicksum
@@ -96,98 +95,10 @@ class alpha:
 print("Hovercraft:")
 equation0 = "(1-math.cos(etheta))/(k0)"
 
-=======
-
-# from fractions import Fraction
-
-def add(first, second):
-    #first/second: float
-    return first + second
-
-def multiply(first, second):
-    #first/second: float
-    return first * second
-
-def divide(first, second):
-    return first / second
-
-def subtract(first, second):
-    return first - second
-
-def exponential(base, exponent):
-    #base: String
-    #exponent: float
-    # return float(sum(Fraction(s) for s in base.split()))**exponent
-    return base**exponent
-        # need lower and upper bounds for er (etheta)
-        #er has user provided bound
-        # beta(er) has our bounds
-        # see if can return 
-
-# #USER INPUTS CONSTANTS/VARIABLES
-# class Constants: #to set constants values
-#     c = 0.5
-#     k0 = 100
-
-# class Variables: #to set constaints (for now only er)
-#     er_min = 0
-#     er_max = 2*math.pi
-
-
-
-
-class constant:
-    def __init__(self, name : str, value : float):
-        self.value = value
-        self.name = name
-
-class variable:
-    def __init__(self, name : str, bounds = [None, None]): #MAY NEED TO CHANGE BOUNDS = NONE (for inf stuff)
-        self.name = name
-        self.bounds = bounds
-
-# k = constant(100, 'k')
-# theta_err = variable([0, 2*pi], 'e_theta')
-
-class lyapunov:
-    def __init__(self, alpha, beta):
-        self.alpha = alpha
-        self.beta = beta
-
- 
-class beta:
-    def __init__(self, variables, constants, equation): #variables/constants are list of respective class, equation is string
-        
-        self.const_names = [const.name for const in constants]
-        self.const_val = [const.value for const in constants] #gets the values in order of const name
-
-
-        self.var_names = [var.name for var in variables]
-        self.var_bounds = [var.bounds for var in variables] #bounds in order of const name
-        self.init_guess = [(var.bounds[0]+var.bounds[1]-(var.bounds[0]+var.bounds[1])/100)/2 for var in variables] #min of the bounds for init guess
-        self.beta = eval(f"lambda {', '.join(self.const_names + self.var_names)}: {equation}") #joins const + variables EX: ['x','y'] -> "x, y"
-
-        models = ["L-BFGS-B", "SLSQP", "Powell", "TNC", "Nelder-Mead", "COBYLA"]
-        self.computed_var_min = opt.minimize(lambda vars: self.beta(*self.const_val, *vars), self.init_guess, bounds=self.var_bounds, method = 'Powell') #can test with diff methods
-        self.computed_var_max = opt.minimize(lambda vars: -self.beta(*self.const_val, *vars), self.init_guess, bounds=self.var_bounds, method = 'Powell')
-        
-
-        self.minimum = self.beta(*self.const_val, *self.computed_var_min.x)
-        self.maximum = self.beta(*self.const_val, *self.computed_var_max.x)
-
-
-# class alpha:
-#     def __init__(self, constants, variables, equation)\
-print("Hovercraft:")
-equation0 = "(1-math.cos(etheta))/(k0)"
-
-# c = constant("c", 0.5)
->>>>>>> 76f8a3b4a3a565d1137d66d1b86ec9289cf69bac
 k0 = constant("k0",1)
 etheta = variable( "etheta", [0,2*math.pi])
 print(f"With beta as {equation0}")
 print(f"etheta bounds: {etheta.bounds}")
-<<<<<<< HEAD
 print(f"k0: {k0.value}")
 func = beta([etheta],[k0], equation0)
 
@@ -200,18 +111,6 @@ ey = variable("ey")
 alpha_hov = alpha(c, [ex, ey], "ep[0]**2+ep[1]**2")
 lyap_hov = lyapunov(alpha_hov, func, 0.01)
 
-=======
-# print(f"c: {c.value}")
-print(f"k0: {k0.value}")
-# func = beta([etheta],[c, k0], "1/2*(1-math.cos(etheta))/(c*k0)")
-func = beta([etheta],[k0], equation0)
-#hovercraft
-
-print("Min: ", func.minimum, " Max: ", func.maximum)
-
-# print(func.computed_var_min)
-# print()
->>>>>>> 76f8a3b4a3a565d1137d66d1b86ec9289cf69bac
 print("==============================================")
 print("Robot:")
 equation = "(1/(2*(1+ec/a)))*(ec**2+es**2)"
@@ -248,11 +147,4 @@ for const in [k1, k2, k3]:
 
 auv = beta([phi, theta, psi],[k1, k2, k3], equation2)
 
-<<<<<<< HEAD
-=======
-# print(f"With beta as {equation}")
-# print(f"ec bounds: {ec.bounds}")
-# print(f"es bounds: {es.bounds}")
-# print(f"a: {a.value}")
->>>>>>> 76f8a3b4a3a565d1137d66d1b86ec9289cf69bac
 print("Min: ", auv.minimum, " Max: ", auv.maximum)
