a27ecf92db76a7cad074af423b13e1e5c537d633
Erik Wu <erik.wu@gmx.de>
2024-08-14 17:33:29 +0200

    small  test

--- factest/plotting/plot_polytopes.py
+++ factest/plotting/plot_polytopes.py
@@ -4,19 +4,20 @@ from matplotlib.patches import Polygon
 from scipy.spatial import ConvexHull
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-def plotPoly(poly, ax = None, color = 'red'): # Takes in tulip polytope and plots it
+
+def plotPoly(poly, ax=None, color='red'):  # Takes in tulip polytope and plots it
     if ax == None:
-        fig,ax = plt.subplots()
+        fig, ax = plt.subplots()
 
     if type(poly) != list:
         poly_verts = pc.extreme(poly)
-        polyPatch = Polygon(poly_verts, facecolor = color, edgecolor = color, alpha = 0.25)
+        polyPatch = Polygon(poly_verts, facecolor=color, edgecolor=color, alpha=0.25)
         ax.add_patch(polyPatch)
 
     else:
         for polygon in poly:
             poly_verts = pc.extreme(polygon)
-            polyPatch = Polygon(poly_verts, facecolor = color, edgecolor = color, alpha = 0.25)
+            polyPatch = Polygon(poly_verts, facecolor=color, edgecolor=color, alpha=0.25)
             ax.add_patch(polyPatch)
 
 
@@ -32,3 +33,31 @@ def plotPoly_3d(poly, ax, color='r'):
             tri.set_facecolor(color)
             tri.set_alpha(0.5)
             ax.add_collection3d(tri)
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    import polytope as pc
+    from scipy.spatial import ConvexHull
+    from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+    # Create a 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Define a polytope (example)
+    A = np.array([[-1, 0, 0],
+                  [1, 0, 0],
+                  [0, -1, 0],
+                  [0, 1, 0],
+                  [0, 0, -1],
+                  [0, 0, 1]])
+    b = np.array([[0], [1], [0], [1], [0], [1]])
+    poly = pc.Polytope(A, b)
+
+    # Plot the polytope
+    plotPoly_3d(poly, ax, color='g')
+
+    # Show plot
+    plt.show()
