6c7ad39c43f52879ef1e45b79bea674579145404
Erik Wu <erik.wu@gmx.de>
2024-09-12 08:33:55 +0200

    working plot mulitple paths (not polished)

--- +LLM/envs/plot_env.py
+++ +LLM/envs/plot_env.py
@@ -39,21 +39,33 @@ def plot_env(title, workspace, G, Theta, O, save=False, plot3d=False, dir='./plo
         i += 1
 
     if path:
-        xref_1 = [xval[0] for xval in path]
-        xref_2 = [xval[1] for xval in path]
-        ax.plot(xref_1, xref_2, 'o-', linewidth=1)
+        if isinstance(path, List):
+            for idx, p in enumerate(path):
+                xref_1 = [xval[0] for xval in p]
+                xref_2 = [xval[1] for xval in p]
+                # use differnt colors for different paths
+                ax.plot(xref_1, xref_2, 'o-', linewidth=1, label=f'Path {idx + 1}')
+            #  add legend
+            ax.legend()
+
+        else:
+            xref_1 = [xval[0] for xval in path]
+            xref_2 = [xval[1] for xval in path]
+            ax.plot(xref_1, xref_2, 'o-', linewidth=1)
 
     ax.autoscale_view()
     # plt.axis('equal')
     plt.title(title)
-
+    # hide axis
+    ax.axis('off')
     if show:
         plt.show()
 
     if save:
         os.makedirs(dir, exist_ok=True)
         path = os.path.join(dir, title)
-        fig.savefig(path)
+
+        fig.savefig(path, bbox_inches='tight')
 
     return fig, ax
 
@@ -86,16 +98,33 @@ def plotPoly_3d(poly, ax, color='r'):
 
 if __name__ == "__main__":
     # plot all the environments in this directory
-    # import all the environments
-    import importlib.util
+    from envs.maze_2d import Theta, G, O, workspace
 
-    save = True
+    # import importlib.util
+    #
+    # save = True
+    #
+    # for env in os.listdir('.'):
+    #     if env.endswith('.py') and env != '__init__.py' and env != 'plot_env.py' and "3d" not in env:
+    #         spec = importlib.util.spec_from_file_location(env, env)
+    #         module = importlib.util.module_from_spec(spec)
+    #         spec.loader.exec_module(module)
+    #
+    #         print('plotting ', env)
+    #         plot_env(module.title, module.workspace, module.G, module.Theta, module.O, save, dir='./plots/manual/')
+    # concatenate paths in array [(0.5, 3.5), (1.5, 3.5), (3.4, 4.45), (4.5, 4.45), (6.5, 4.75)]
+    # [(0.5, 3.5), (1.5, 3.5), (2.8, 3.5), (2.8, 4.2), (3.4, 4.45), (4.5, 4.45), (6.5, 4.75)]
+    # [(0.5, 3.5), (1.5, 3.5), (2.8, 3.5), (3.4, 3.5), (3.4, 4.45), (4.5, 4.45), (6.5, 4.75)]
+    # [(0.5, 3.5), (1.5, 3.5), (2.8, 3.5), (2.8, 2.95), (3.4, 2.95), (3.4, 2.0), (4.5, 2.0), (5.5, 2.0), (5.5, 4.75), (6.5, 4.75)]
+    # [(0.5, 3.5), (1.5, 3.5), (2.8, 3.5), (2.8, 4.0), (3.4, 4.0), (3.4, 4.45), (4.5, 4.45), (5.5, 4.45), (6.5, 4.75)]
 
-    for env in os.listdir('.'):
-        if env.endswith('.py') and env != '__init__.py' and env != 'plot_env.py' and "3d" not in env:
-            spec = importlib.util.spec_from_file_location(env, env)
-            module = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(module)
+    paths = [[(0.5, 3.5), (1.5, 3.5), (3.4, 4.45), (4.5, 4.45), (6.5, 4.75)],
+             [(0.5, 3.5), (1.5, 3.5), (2.8, 3.5), (2.8, 4.2), (3.4, 4.45), (4.5, 4.45), (6.5, 4.75)],
+             [(0.5, 3.5), (1.5, 3.5), (2.8, 3.5), (3.4, 3.5), (3.4, 4.45), (4.5, 4.45), (6.5, 4.75)],
+             [(0.5, 3.5), (1.5, 3.5), (2.8, 3.5), (2.8, 2.95), (3.4, 2.95), (3.4, 2.0), (4.5, 2.0), (5.5, 2.0),
+              (5.5, 4.75), (6.5, 4.75)],
+             [(0.5, 3.5), (1.5, 3.5), (2.8, 3.5), (2.8, 4.0), (3.4, 4.0), (3.4, 4.45), (4.5, 4.45), (5.5, 4.45),
+              (6.5, 4.75)]
 
-            print('plotting ', env)
-            plot_env(module.title, module.workspace, module.G, module.Theta, module.O, save, dir='./plots/manual/')
+             ]
+    plot_env('Maze 2D', workspace, G, Theta, O, save=True, dir='./paper', plot3d=False, show=True, path=paths)
